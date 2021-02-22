ab093fad52c3f0782e12fb58bbd3c33ad4f05191
milad <miladxgh@outlook.com>
2021-02-22 14:30:58 +0330

    toggle light/dark mode

--- theme.js
+++ theme.js
@@ -1,27 +1,22 @@
-/*
-let inputElements = document.getElementsByClassName("input-text");
-let cardElement = document.querySelector(".card");
-let buttonElements = document.getElementsByClassName("button");
+// DOM
+let buttons = document.getElementsByClassName('button');
+let logo = document.getElementById('logo');
 
-const dark_bg = '#292E35';
-const dark_bg_dark_shadow = '#21252a';
-const dark_bg_light_shadow = '#313740';
-const primary_dark = '#4526f9';
-
-// buttonElements.addEventListener("click", darkmode);
-document.body.style.background = dark_bg;
-
-cardElement.style.color = 'white';
-cardElement.style.background = dark_bg;
-cardElement.style.boxShadow =`4px 4px 8px ${dark_bg_dark_shadow},-4px -4px 8px ${dark_bg_light_shadow}`;
-
-for (let i in inputElements) {
-    inputElements[i].style.background = dark_bg;
-    inputElements[i].style.boxShadow =`0 0 0 ${dark_bg},0 0 0 ${dark_bg},inset  4px 4px 8px ${dark_bg_dark_shadow},inset -4px -4px 8px ${dark_bg_light_shadow}`;
+// set Theme
+function setTheme(themeName) {
+    document.body.className = themeName;
+    
 }
 
+buttons[0].addEventListener("click", toggleTheme);
 
-for (let i in buttonElements) {
-    buttonElements[i].style.background = dark_bg;
+function toggleTheme() {
+    if (document.body.className === 'light-mode') {
+        setTheme('dark-mode');
+        logo.style.filter = "invert()";
+
+    } else {
+        setTheme('light-mode');
+        logo.style.filter = "invert(0)";
+    }
 }
-*/
